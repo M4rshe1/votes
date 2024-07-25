031259694183e8b9d33199a08f12ea9425d645fe
M4rshe1 <colin.heggli@gmail.com>
2024-07-25 18:03:16 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['@prisma/client', 'bcrypt']
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push({
+            test: /\.html$/,
+            use: ['null-loader'], // Use null-loader to ignore .html files
+        });
+        return config;
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,20 @@ const config: Config = {
     },
   },
   plugins: [],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#9d2328",
+        },
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#9d2328",
+        },
+      }
+    ],
+  },
+  darkMode: ['class', '[data-theme="dark"]']
 };
 export default config;
